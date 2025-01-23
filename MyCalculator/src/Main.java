ad8b53ac54051757b7bc1ccace44885111d311a9
Patatazz <cjeraldluise@gmail.com>
2025-01-23 15:19:21 +0800

    ...

--- MyCalculator/src/Main.java
+++ MyCalculator/src/Main.java
@@ -6,19 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		JFrame frame = new JFrame();
-		
-		CalcuButton test = new CalcuButton();
-		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-		frame.setSize(500,500);
-		frame.setLayout(new FlowLayout());
-
-		// to be use on CalculatorUI class
-		frame.add(test.getButton(4));
-	
-//		CalculatorUI test = new CalculatorUI();
+		CalculatorUI test = new CalculatorUI();
 		
 
 	}
